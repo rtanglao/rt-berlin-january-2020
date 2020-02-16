7a68eb34e1592841c571ccc8bd62d9b8f9509125
Roland Tanglao <roland@rolandtanglao.com>
2020-02-15 18:48:19 -0800

    skip if dominant colour is undefined

--- print-file-colorname-hex-color.rb
+++ print-file-colorname-hex-color.rb
@@ -13,7 +13,11 @@ ARGF.each_line do |line|
 	file = line.chomp	
 	dominant_colour = Colorname.find_by_image(file)[0].to_s
 	logger.debug("file:" + file)
-	logger.debug("dominant_colour" + dominant_colour)
+	logger.debug("dominant_colour:" + dominant_colour)
+	if dominant_colour == "undefined"
+		logger.debug "SKIPPING because dominant colour is nil"
+		next
+	end
 	hex_colour = Color::CSS[dominant_colour].html
 	printf("%s,%s,%s\n", file, dominant_colour, hex_colour)
 end
